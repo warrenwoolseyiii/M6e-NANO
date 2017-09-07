b322642a3ca5ac63fc8bc628508e16a607b1302d
warrenwoolseyiii <warren.woolseyiii@gmail.com>
2017-09-06 22:35:37 -0700

    Added the new atmel studio project

---
+++ mobarmor_rfid/mobarmor_rfid/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * mobarmor_rfid.cpp
+ *
+ * Created: 9/6/2017 10:35:19 PM
+ * Author : warre
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
