28b1b5fa7ab15a6b78dc07f6e6bdfd2dfa7d06ab
Warren Woolsey <warren.woolseyiii@gmail.com>
2017-09-30 21:53:43 -0700

    Added the thingmagic_mercuryAPI project and associated source files. Got it to compile in 30 minutes so I am tipping my hat to the guy that wrote the porting guide :)

---
+++ thingmagic_mercuryAPI/thingmagic_mercuryAPI/Debug/makedep.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+hex_bytes.c
+
+osdep_dummy.c
+
+serial_reader.c
+
+serial_reader_l3.c
+
+serial_transport_dummy.c
+
+tmr_param.c
+
+tmr_strerror.c
+
+tmr_utils.c
+
+tm_reader.c
+
