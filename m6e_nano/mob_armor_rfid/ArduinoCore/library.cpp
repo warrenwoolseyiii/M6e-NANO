8056876f2ccfb548b4cf57fc7a30c238bb0e91b1
warrenwoolseyiii <warren.woolseyiii@gmail.com>
2017-10-15 00:42:32 -0700

    Created an atmel studio project off a base .ino file for arduino. Lets get this shit moving.

---
+++ m6e_nano/mob_armor_rfid/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 10/15/2017 12:35:17 AM
+ * Author : warre
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ m6e_nano/mob_armor_rfid/mob_armor_rfid/Debug/makedep.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+..\..\..\submodules\SparkFun_Simultaneous_RFID_Tag_Reader_Library\src\SparkFun_UHF_RFID_Reader.cpp
+
+Sketch.cpp
+
