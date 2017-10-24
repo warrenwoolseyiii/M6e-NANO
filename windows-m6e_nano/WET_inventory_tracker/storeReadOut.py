bf50ef11fe18d082775cca2bb8c71f0016f651d5
warrenwoolseyiii <warren.woolseyiii@gmail.com>
2017-10-23 21:44:04 -0700

    Broke the storage algo out into functions

--- windows-m6e_nano/WET_inventory_tracker/storeReadOut.py
+++ windows-m6e_nano/WET_inventory_tracker/storeReadOut.py
@@ -1,19 +1,47 @@
 import re
 
-readF = "readOut.txt"
-csvF = "storageTable.csv"
-with open(readF, 'r') as f:
-   csvOut = open(csvF, "a+")
-   for line in f:
-      words = re.split("(\s+)", line)
-      csvStr = ""
-      for w in words:
-         phrases = re.split("(:)", w)
-         if len(phrases) >= 2:
-            csvStr += phrases[2]
-            csvStr += ","
-      if csvStr != "":
-         csvStr += "\n"
-         csvOut.write(csvStr)
-f.close()
-csvOut.close()
+def storeNewValues( newValueFName, csvStorageFName ):
+   """ stores properly formatted values from the newValueFName file into the csvStorageFName file.
+   Assumes that newValueFName is a text file output by the thingmagic library. Assumes that csvStorageFName
+   is a csv file.
+
+   """
+   txtIn = open(newValueFName, 'r')
+   csvOut = open(csvStorageFName, "a+")
+
+   for entries in txtIn:
+      csvOutStr = parseNewEntry(entries)
+      csvOut.write(csvOutStr)
+
+   txtIn.close()
+   csvOut.close()
+   return
+
+
+def parseNewEntry( entry ):
+   """ parses a line from a text file (an entry) and returns a CSV formatted string of the various phrases in the entry """
+   categories = re.split("(\s+)", entry)
+   csvStr = ""
+
+   for phrases in categories:
+      words = re.split("(:)", phrases)
+      if verifyPhrase(words):
+         csvStr += words[2]
+         csvStr += ","
+   
+   csvStr += "\n"
+   return csvStr
+
+def verifyPhrase( words ):
+   """ verifies that a phrase in an entry is in the correct format which is [title]:[value]
+   returns true if the format is correct and false otherwise
+
+   """
+   if len(words) != 3:
+      return False
+   elif words[1] != ":":
+      return False
+   return True
+
+# run the process
+storeNewValues("readOut.txt", "storageTable.csv")
