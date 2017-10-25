b36c3db18208c0fe23c67f77755b1c40c57733a3
Warren Woolsey <warren.woolseyiii@gmail.com>
2017-10-25 09:26:24 -0700

    Refined the sorting algorithm. Now we read in the current values, pull in the list of EPCs and use set mathematics to filter duplicates.

--- windows-m6e_nano/WET_inventory_tracker/storeReadOut.py
+++ windows-m6e_nano/WET_inventory_tracker/storeReadOut.py
@@ -1,64 +1,74 @@
 import re
 import csv
+import sys
 
-gFieldNames = ["EPC", "Antenna", "Count", "Time", "Protocol", "CRC", "PC"]
-gEPCSet = set()
+gEPCFieldNames = ["EPC"]
 
-def createNewStorageTable( csvStorageFName ):
-   with open(csvStorageFName, "w") as csvFile:
-      writer = csv.DictWriter(csvFile, fieldnames=gFieldNames)
-      writer.writeheader()
+def updateStorageTable( csvStorageFName, epcSet ):
+      with open(csvStorageFName, "w") as csvFile:
+            writer = csv.DictWriter(csvFile, fieldnames=gEPCFieldNames)
+            writer.writeheader()
+            for items in epcSet:
+                  writer.writerow({gEPCFieldNames[0]:items})
+            csvFile.close()
+      return
+      
 
 def storeNewValues( newValueFName, csvStorageFName ):
-   with open(newValueFName, 'r') as txtIn:
-      with open(csvStorageFName, "a+") as csvOut:
-         for entries in txtIn:
-            values = parseNewEntry(entries)
-            if len(values) >= 7:
-               writer = csv.DictWriter(csvOut, fieldnames=gFieldNames)
-               writer.writerow({gFieldNames[0]:values[0], gFieldNames[1]:values[1], gFieldNames[2]:values[2], gFieldNames[3]:values[3], gFieldNames[4]:values[4], gFieldNames[5]:values[5], gFieldNames[6]:values[6]})
-   return
+      epcList = getEPCList(csvStorageFName)
+      epcSet = createSetFromStorageTable(epcList)
+      print(epcSet)
+      with open(newValueFName, 'r') as txtIn:
+            newEntriesList = []
+            for entries in txtIn:
+                  values = parseNewEntry(entries)
+                  if len(values) >= 1:
+                        newEntriesList.append(values[0])
+            txtIn.close()
+      newEntriesSet = set(newEntriesList)
+      epcSet.update(newEntriesSet)
+      print(epcSet)
+      updateStorageTable(csvStorageFName, epcSet)
+      return
 
 def parseNewEntry( entry ):
-   categories = re.split("(\s+)", entry)
-   csvFields = []
-
-   for phrases in categories:
-      words = re.split("(:)", phrases)
-      if verifyPhrase(words):
-         csvFields.append(words[2])
-   
-   return csvFields
+      categories = re.split("(\s+)", entry)
+      fields = []
+      for phrases in categories:
+            words = re.split("(:)", phrases)
+            if verifyPhrase(words):
+                  fields.append(words[2])
+      return fields
 
 def verifyPhrase( words ):
-   if len(words) < 3:
-      return False
-   elif words[1] != ":":
-      return False
-   return True
+      if len(words) < 3:
+            return False
+      elif words[1] != ":":
+            return False
+      return True
 
 def getEPCList( csvStorageFName ):
-   with open(csvStorageFName, "r") as csvStorageTable:
-      reader = csv.DictReader(csvStorageTable, fieldnames=gFieldNames)
       epcs = []
-      index = 0
-      for row in reader:
-         # skip the header!
-         if index > 0:
-            epcs.append(row["EPC"])
-         index = index + 1
-   
-   return epcs
+      try:
+            with open(csvStorageFName, "r") as csvStorageTable:
+                  reader = csv.DictReader(csvStorageTable, fieldnames=gEPCFieldNames)
+                  index = 0
+                  for row in reader:
+                        # skip the header!
+                        if index > 0:
+                              epcs.append(row["EPC"])
+                        index = index + 1
+
+                  csvStorageTable.close()
+      except:
+            e = sys.exc_info()[0]
+            print(e)
+      return epcs
 
 def createSetFromStorageTable( EPCList ):
-   epc = set(EPCList)
+      epc = set(EPCList)
+      return epc
 
 # run the process
-#storeNewValues("readOut.txt", "storageTable.csv")
-#createNewStorageTable("storageTable.csv")
-epcList = getEPCList("storageTable.csv")
-createSetFromStorageTable(epcList)
-gEPCSet.update(epcList)
-print(gEPCSet)
-print("Inventory count: ", len(gEPCSet))
+storeNewValues("readOut.txt", "storageTable.csv")
 
